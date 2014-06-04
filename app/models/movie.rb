ec614c10e30db9c1ceb46c42d01d36087815a1eb
Kristine Yefimtseva <ke@anahoret.com>
2014-06-04 13:12:49 +0300

    validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ActiveRecord::Base
   validates :title, presence: true
-  validates :rating, presence: true
+  validates :rating, presence: true, inclusion: { in: ['G','PG','PG-13','R', 'NC-17'], allow_blank: true }
+  validates :release_date, presence: { message: "looks bad" }
 
   def self.all_ratings
     Movie.select(:rating).distinct.pluck(:rating)

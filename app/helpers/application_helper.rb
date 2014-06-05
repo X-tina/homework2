c0c56a08f9140e25d5fe12f0725562f4f1589fa7
Kristine Yefimtseva <ke@anahoret.com>
2014-06-05 13:51:20 +0300

    Fix change direction by_links without arrow

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
   def sortable(column, title = nil)
+    p "!!!!", title
     title ||= column.titleize
     direction = (column == params[:movies_sort] && params[:direction] == "ASC") ? "DESC" : "ASC"
-    link_to title, :movies_sort => column, :direction => direction.to_s
+    link_to title, movies_path(:movies_sort => column, :direction => direction.to_s)
   end
 end

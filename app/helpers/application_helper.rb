43e1982e57d94ef5bef006742705039db1ae1e9e
Kristine Yefimtseva <ke@anahoret.com>
2014-06-06 10:56:00 +0300

    Add links to the title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,6 @@ module ApplicationHelper
     title ||= column.titleize
     direction = (column == params[:movies_sort] && params[:direction] == "ASC") ? "DESC" : "ASC"
     (params[:direction] == "ASC") ? arrow = "&darr;".html_safe : arrow = "&uarr;".html_safe
-    #title += ' ' + "#{arrow}"
     link_to "#{title} #{arrow}".html_safe, movies_path(:movies_sort => column, :direction => direction.to_s)
   end
 end

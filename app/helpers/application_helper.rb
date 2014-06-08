e3131ccacf30df9189893b200967b80c7081a410
Kristine <kristinka89@gmail.com>
2014-06-08 20:05:43 +0300

    Add scope and fix sorting params

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,8 @@
 module ApplicationHelper
   def sortable(column, title = nil)
-    p "!!!!", title
     title ||= column.titleize
     direction = (column == params[:movies_sort] && params[:direction] == "ASC") ? "DESC" : "ASC"
     (params[:direction] == "ASC") ? arrow = "&darr;".html_safe : arrow = "&uarr;".html_safe
-    link_to "#{title} #{arrow}".html_safe, movies_path(:movies_sort => column, :direction => direction.to_s)
+    link_to "#{title} #{arrow}".html_safe, :movies_sort => column, :direction => direction.to_s
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,11 @@ class Movie < ActiveRecord::Base
   def self.all_ratings
     @all_ratings = %W[G PG PG-13 NC-17 R]
   end
+
+  scope :list, ->(options) {
+    res = all
+    res = res.where(rating: options[:rating]) if options.key? :rating
+    res = res.order(options[:order]) if options.key? :order
+    res
+  }
 end

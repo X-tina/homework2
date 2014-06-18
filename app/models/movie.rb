98e3b7cab1b9e06f12356eeb7f136aec9ebb499a
Kristine Yefimtseva <ke@anahoret.com>
2014-06-18 10:46:43 +0300

    Add validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,10 @@
 class Movie < ActiveRecord::Base
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment :avatar,
+  :content_type => { :content_type => "image/jpeg" },
+  :size => { :in => 0..5.megabytes }
+
   validates :title, presence: true, uniqueness: true
   validates :rating, presence: true, format: { with: /\AG|PG|PG-13|NC-17|R\z/ }, length: { minimum: 1 }
 
